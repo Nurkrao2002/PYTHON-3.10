4e4032ab25f16593781df0e724e54586f1006e98
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2022-11-09 20:20:49 +0530

    Update 01_Data_Type_Integer.py

--- 01_Data_Types/01_Data_Type_Integer.py
+++ 01_Data_Types/01_Data_Type_Integer.py
@@ -1,17 +1,17 @@
 """Data_Type_Integer"""
 
-# The variable name cannot start with a number. It can only start with a character or an underscore.
-# No special characters are allowed.
-# Variables in python are case sensitive.
-# They can only contain alpha-numeric characters and underscores.
-# Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+The variable name cannot start with a number. It can only start with a character or an underscore.
+No special characters are allowed.
+Variables in python are case sensitive.
+They can only contain alpha-numeric characters and underscores.
+Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
 
 
 print(3409)         # int
 print(789354)       # int
 print(7220092002)   # int
 print(45678)        # int
-# _39872987        # NameError: name '_39872987' is not defined
+_39872987        # NameError: name '_39872987' is not defined
 
 ''' to find the type of Data_Type '''
 
@@ -29,4 +29,3 @@ print(12//4.0)
 """ squareoff """
 
 print(5**4)
-
